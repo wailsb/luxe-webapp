2f8587d94de9e62e2144f4eae718a4a19fe499ea
wail <wail.saribey@gmail.com>
2025-06-29 22:38:19 +0100

    fixed navbar

--- src/Components/phone/NavbarPhone.tsx
+++ src/Components/phone/NavbarPhone.tsx
@@ -11,8 +11,8 @@ export default function NavbarPhone() {
 
     function calculateWidth() {
         const ScreenWidth = window.innerWidth;
-        setHeight((ScreenWidth * 10) / 100);
-        return (ScreenWidth * 15) / 100;
+        setHeight((ScreenWidth * 15) / 100); // Increased logo height
+        return (ScreenWidth * 22) / 100;     // Increased logo width
     }
 
     useEffect(() => {
@@ -29,30 +29,46 @@ export default function NavbarPhone() {
 
     return (
         <nav className="flex flex-col justify-center">
-            <div className="flex justify-center">
+            <div className="flex justify-center h-24"> {/* Increased navbar height */}
                 <div className="flex flex-1 justify-start items-center">
-                    <div className="ml-5" onClick={() => setSideOpen(true)}>
-                        <Image src="/icons/list.png" alt="Menu" style={{ objectFit: "contain", width: `15px`, height: `15px` }} width={15} height={15} />
+                    <div className="ml-8" onClick={() => setSideOpen(true)}>
+                        <Image
+                            src="/icons/list.png"
+                            alt="Menu"
+                            style={{ objectFit: "contain", width: `32px`, height: `32px` }}
+                            width={32}
+                            height={32}
+                        />
                     </div>
                 </div>
-                <Link href="/" className="items-center">
-                    <Image src="/logo.png" alt="Description" style={{ objectFit: "contain", width: `${widthNav}px`, height: `${heightNav}px` }} width={widthNav} height={heightNav} />
+                <Link href="/" className="items-center flex">
+                    <Image
+                        src="/logo.png"
+                        alt="Description"
+                        style={{
+                            objectFit: "contain",
+                            width: `${widthNav}px`,
+                            height: `${heightNav}px`
+                        }}
+                        width={widthNav}
+                        height={heightNav}
+                    />
                 </Link>
                 <div className="flex flex-1"/>
             </div>
             {/* Side Navbar */}
             <div
-                className={`fixed top-0 left-0 h-full w-2/3 max-w-xs bg-white shadow-lg z-50 transform transition-transform duration-300 ${
+                className={`fixed top-0 left-0 h-full w-4/5 max-w-md bg-white shadow-lg z-50 transform transition-transform duration-300 ${
                     sideOpen ? "translate-x-0" : "-translate-x-full"
-                } flex flex-col pt-20`}
-                style={{ minWidth: "200px" }}
+                } flex flex-col pt-28`} // More padding top for bigger navbar
+                style={{ minWidth: "260px" }}
             >
-                <button className="self-end mr-4 mb-4" onClick={() => setSideOpen(false)}>
-                    <span className="text-2xl">&times;</span>
+                <button className="self-end mr-8 mb-8" onClick={() => setSideOpen(false)}>
+                    <span className="text-4xl">&times;</span>
                 </button>
-                <Link className="py-2 px-5 text-black" href="/Products" onClick={() => setSideOpen(false)}>Catalog</Link>
-                <Link className="py-2 px-5 text-black" href="/" onClick={() => setSideOpen(false)}>Tos</Link>
-                <Link className="py-2 px-5 text-black" href="/Contact" onClick={() => setSideOpen(false)}>Contact</Link>
+                <Link className="py-5 px-8 text-xl text-black" href="/Products" onClick={() => setSideOpen(false)}>Catalog</Link>
+                <Link className="py-5 px-8 text-xl text-black" href="/" onClick={() => setSideOpen(false)}>Tos</Link>
+                <Link className="py-5 px-8 text-xl text-black" href="/Contact" onClick={() => setSideOpen(false)}>Contact</Link>
             </div>
             {/* Optional overlay */}
             {sideOpen && (
